41b79c8865bcbfe6864686593916744cab302911
itschoolkw0103 <itschoolkw0103@gmail.com>
2025-01-07 14:36:29 +0900

    20250108

--- my-next-project/app/page.tsx
+++ my-next-project/app/page.tsx
@@ -1,15 +1,9 @@
 import styles from "./page.module.css";
 import Image from "next/image";
 
-type News = {
-  id: string;
-  title: string;
-  category: {
-    name: string;
-  };
-  publishedAt: string;
-  createdAt: string;
-};
+import NewsList from "./_components/NewsList";
+import ButtonLink from "@/app/_components/ButtonLink";
+import { News } from "@/app/_libs/microcms";
 
 const data: { contents: News[] } = {
   contents: [
@@ -45,21 +39,31 @@ const data: { contents: News[] } = {
 
 export default function Home() {
   const name = "世界";
+  const sliceData = data.contents.slice(0, 2);
   return (
-    <section className={styles.top}>
-      <div>
-        <h1 className={styles.title}>テクノロジーの力で{name}を変える</h1>
-        <p className={styles.description}>
-          私たちは市場をリードしているグローバルテックカンパニーです。
-        </p>
-      </div>
-      <Image
-        className={styles.bgimg}
-        src="/img-mv.jpg"
-        alt=""
-        width={4000}
-        height={1200}
-      />
-    </section>
+    <>
+      <section className={styles.top}>
+        <div>
+          <h1 className={styles.title}>テクノロジーの力で{name}を変える</h1>
+          <p className={styles.description}>
+            私たちは市場をリードしているグローバルテックカンパニーです。
+          </p>
+        </div>
+        <Image
+          className={styles.bgimg}
+          src="/img-mv.jpg"
+          alt=""
+          width={4000}
+          height={1200}
+        />
+      </section>
+      <section className={styles.news}>
+        <h2 className={styles.newsTitle}>News</h2>
+        <NewsList news={sliceData} />
+        <div className={styles.newsLink}>
+          <ButtonLink href="/news">もっと見る</ButtonLink>
+        </div>
+      </section>
+    </>
   );
 }
