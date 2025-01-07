1cba03b88714bd8590d0b6290754f23ab46d211f
itschoolkw0103 <itschoolkw0103@gmail.com>
2025-01-07 10:09:10 +0900

    20250107

--- my-next-project/app/page.tsx
+++ my-next-project/app/page.tsx
@@ -1,4 +1,47 @@
 import styles from "./page.module.css";
+import Image from "next/image";
+
+type News = {
+  id: string;
+  title: string;
+  category: {
+    name: string;
+  };
+  publishedAt: string;
+  createdAt: string;
+};
+
+const data: { contents: News[] } = {
+  contents: [
+    {
+      id: "1",
+      title: "渋谷にオフィスを移転しました",
+      category: {
+        name: "更新情報",
+      },
+      publishedAt: "2023/05/19",
+      createdAt: "2023/05/19",
+    },
+    {
+      id: "2",
+      title: "当社CEOが業界リーダーTOP30に選出されました",
+      category: {
+        name: "更新情報",
+      },
+      publishedAt: "2023/05/19",
+      createdAt: "2023/05/19",
+    },
+    {
+      id: "3",
+      title: "テストの記事です",
+      category: {
+        name: "更新情報",
+      },
+      publishedAt: "2023/04/19",
+      createdAt: "2023/04/19",
+    },
+  ],
+};
 
 export default function Home() {
   const name = "世界";
@@ -10,7 +53,13 @@ export default function Home() {
           私たちは市場をリードしているグローバルテックカンパニーです。
         </p>
       </div>
-      <img className={styles.bgimg} src="/img-mv.jpg" alt="" />
+      <Image
+        className={styles.bgimg}
+        src="/img-mv.jpg"
+        alt=""
+        width={4000}
+        height={1200}
+      />
     </section>
   );
 }
